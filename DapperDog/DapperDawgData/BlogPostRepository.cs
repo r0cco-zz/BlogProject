53da4775128d4304896822da882907a92601cf67
Patrick Beauchamp <patty.beauchamp@gmail.com>
2015-12-02 14:46:03 -0500

    added 2 unit tests

--- DapperDog/DapperDawgData/BlogPostRepository.cs
+++ DapperDog/DapperDawgData/BlogPostRepository.cs
@@ -18,41 +18,41 @@ namespace DapperDawgData
         public BlogPostRepository()
         {
             _cn = new SqlConnection(Settings.ConnectionString);
-        } 
+        }
 
         public List<BlogPost> GetAllBlogPosts()
         {
-                var blogPosts = _cn.Query<BlogPost>("GetAllPostsOrderedByDate", commandType: CommandType.StoredProcedure).ToList();
+            var blogPosts =
+                _cn.Query<BlogPost>("GetAllPostsOrderedByDate", commandType: CommandType.StoredProcedure).ToList();
 
-                return blogPosts;
-            }
-        
+            return blogPosts;
+        }
+
         public BlogPost GetBlogPostByID(int id)
         {
-                var p = new DynamicParameters();
-                p.Add("postId", id);
+            var p = new DynamicParameters();
+            p.Add("postId", id);
 
-                var post = _cn.Query<BlogPost>("GetPostByID", p, commandType: CommandType.StoredProcedure).FirstOrDefault();
-                return post;
-            }
-        
+            var post = _cn.Query<BlogPost>("GetPostByID", p, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            return post;
+        }
+
         public List<Tag> GetAllTags()
         {
-                var tags = _cn.Query<Tag>("GetAllTags", commandType: CommandType.StoredProcedure).ToList();
+            var tags = _cn.Query<Tag>("GetAllTags", commandType: CommandType.StoredProcedure).ToList();
 
-                return tags;
-            }
-        
+            return tags;
+        }
+
         public List<Tag> GetTagsByPostID(int id)
         {
-                var p = new DynamicParameters();
-                p.Add("postId", id);
+            var p = new DynamicParameters();
+            p.Add("postId", id);
 
-                var tags = _cn.Query<Tag>("GetAllTagsOnAPostByPostID", p, commandType: CommandType.StoredProcedure).ToList();
+            var tags = _cn.Query<Tag>("GetAllTagsOnAPostByPostID", p, commandType: CommandType.StoredProcedure).ToList();
 
-                return tags;
-            }
-<<<<<<< HEAD
+            return tags;
+
         }
 
         public List<Category> GetAllCategories()
@@ -61,34 +61,33 @@ namespace DapperDawgData
             {
                 return cn.Query<Category>("SELECT c.CategoryID, c.CategoryName FROM Categories c").ToList();
             }
-        } 
+        }
 
-=======
-        
->>>>>>> b2eb7013662190b01ca7de8123e95cfb2ff3cadd
         public string GetCategoryByPostID(int id)
         {
-                var p = new DynamicParameters();
-                p.Add("postId", id);
+            var p = new DynamicParameters();
+            p.Add("postId", id);
 
-                var catname =
-                    _cn.Query<string>("GetCategoryByPostID", p, commandType: CommandType.StoredProcedure)
-                        .FirstOrDefault();
+            var catname =
+                _cn.Query<string>("GetCategoryByPostID", p, commandType: CommandType.StoredProcedure)
+                    .FirstOrDefault();
 
-                return catname;
-            }
-        
+            return catname;
+        }
+
         public void AddNewBlogPost(BlogPost newBlogPost)
         {
             var p = new DynamicParameters();
-            p.Add("categoryID", newBlogPost.CategoryID);
+            p.Add("CategoryID", newBlogPost.CategoryID);
             p.Add("PostTitle", newBlogPost.PostTitle);
-            p.Add("PostDate", newBlogPost.PostDate);
+            p.Add("PostDate", newBlogPost.PostDate, dbType:DbType.Date, direction:ParameterDirection.Input);
             p.Add("PostContent", newBlogPost.PostContent);
             p.Add("Author", newBlogPost.Author);
             p.Add("PostStatus", newBlogPost.PostStatus);
+            p.Add("PostID", p, dbType:DbType.Int32, direction:ParameterDirection.Output);
 
             _cn.Execute("AddNewPost", p, commandType: CommandType.StoredProcedure);
+            int newPostID = p.Get<int>("PostID");
         }
 
         public void AddNewPostTag(int PostId, int TagId)
@@ -104,10 +103,10 @@ namespace DapperDawgData
         {
             var p = new DynamicParameters();
             p.Add("TagName", TagName);
-            p.Add("tagId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("TagId", dbType: DbType.Int32, direction: ParameterDirection.Output);
 
             _cn.Execute("AddNewTag", p, commandType: CommandType.StoredProcedure);
             return p.Get<int>("TagId");
-            }
+        }
     }
 }
--- DapperDog/DapperDawgTests/OrderOperationsTests.cs
+++ DapperDog/DapperDawgTests/OrderOperationsTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DapperDawgBll;
+using DapperDawgData;
 using DapperDawgData.Config;
 using DapperDawgModels;
 using NUnit.Framework;
@@ -30,34 +31,63 @@ namespace DapperDawgTests
             _cn.Dispose();
         }
 
+        //[Test]
+        //public void AddnewBlogPost_ShouldReturnNewPostId()
+        //{
+        //    BlogPostRepository repo = new BlogPostRepository();
+        //    int expected = 0;
+        //    int newPostId = 0;
+
+        //    BlogPost newBlogPost = new BlogPost()
+        //    {
+        //        CategoryID = 2,
+        //        CategoryName = "Pet Health",
+        //        PostTitle = "Teeth",
+        //        PostDate = DateTime.Parse("3-12-2015"),
+        //        PostContent = "teeth",
+        //        Author = "Joe Schmoe",
+        //        PostStatus = 0,
+        //        Tags = new List<Tag>
+        //        {
+        //            new Tag {TagID = 1, TagName = "Dogs"}
+        //        }
+                
+        //    };
+        //    SqlCommand cmd = new SqlCommand();
+        //    cmd.CommandText = "select max(PostID) from posts";
+        //    cmd.Connection = _cn;
+
+        //    expected = int.Parse(cmd.ExecuteScalar().ToString()) + 1;
+
+        //    BlogPostOperations bpo = new BlogPostOperations();
+        //    bpo.AddNewBlogPost(newBlogPost);
+
+        //    newPostId = int.Parse(cmd.ExecuteScalar().ToString());
+
+        //    Assert.AreEqual(expected, newPostId);
+        //}
+
         [Test]
-        public void AddnewBlogPostWithNewTag_ShouldReturnNewTagId()
+        public void CheckNewTagID()
         {
+            BlogPostRepository repo = new BlogPostRepository();
+            int expected = RetrieveLastTagId() + 1;
+            int id = repo.AddNewTag("loopy");
+            Assert.AreEqual(expected, id);
+        }
+
+        [Test]
+        public void CheckGetAllCategories()
+        {
+            BlogPostRepository repo = new BlogPostRepository();
             int expected = 0;
-            int newTagId = 0;
-
-            BlogPost newBlogPost = new BlogPost()
-            {
-                CategoryID = 2,
-                CategoryName = "Pet Health",
-                PostTitle = "Teeth",
-                PostDate = DateTime.Now,
-                PostContent = "teeth",
-                Author = "Joe Schmoe",
-                PostStatus = 0,
-                Tags = new List<Tag>
-                {
-                    new Tag {TagName = "teeth"}
-                }
-            };
-
-            expected = RetrieveLastTagId() + 1;
-
-            BlogPostOperations bpo = new BlogPostOperations();
-            bpo.AddNewBlogPost(newBlogPost);
-
-            newTagId = RetrieveLastTagId();
-            Assert.AreEqual(expected, newTagId);
+            int result = 0;
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select max(categoryid) from categories";
+            cmd.Connection = _cn;
+            expected = int.Parse(cmd.ExecuteScalar().ToString());
+            result = repo.GetAllCategories().Count;
+            Assert.AreEqual(expected,result);
         }
 
         public int RetrieveLastTagId()
