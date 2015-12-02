97cc6c4bd12b0e8357ef3cad5d4e1fc3644d970e
Chary Gurney <chary.gurney@gmail.com>
2015-12-02 15:13:15 -0500

    another merge conflict fixed

--- DapperDog/DapperDawgBll/BlogPostOperations.cs
+++ DapperDog/DapperDawgBll/BlogPostOperations.cs
@@ -47,22 +47,6 @@ namespace DapperDawgBll
         {
             _repo.AddNewBlogPost(newBlogPost);
             //var tagList = _repo.GetAllTags();
-
-
-            foreach (var tag in newBlogPost.Tags)
-            {
-                if (tagList.Contains(tag))
-                {
-                    _repo.AddNewPostTag(newBlogPost.PostID, tag.TagID);
-                }
-                else
-                {
-                  int tagId =  _repo.AddNewTag(tag.TagName);
-                    _repo.AddNewPostTag(newBlogPost.PostID, tagId);
-                }   
-            }
-
-=======
             //foreach (var tag in newBlogPost.Tags)
             //{
             //    if (tagList.Contains(tag))
@@ -75,7 +59,7 @@ namespace DapperDawgBll
             //        _repo.AddNewPostTag(newBlogPost.PostID, tagId);
             //    }   
             //}
->>>>>>> 53da4775128d4304896822da882907a92601cf67
+
         }
     }
 }
