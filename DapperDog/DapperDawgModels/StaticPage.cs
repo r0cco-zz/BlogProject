4ce1556a629ad633417dfec54a419f45958c33c6
Patrick Beauchamp <patty.beauchamp@gmail.com>
2015-12-03 13:48:44 -0500

    Added some get posts methods

---
+++ DapperDog/DapperDawgModels/StaticPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDawgModels
+{
+     public class StaticPage
+    {
+        public int StaticPageID { get; set; }
+        public string StaticPageTitle { get; set; }
+        public string StaticPageContent { get; set; }
+    }
+}
