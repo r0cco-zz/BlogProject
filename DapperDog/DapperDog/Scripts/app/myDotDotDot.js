ecfcc86a009f1970b2f8ba55df10d19cd4591b6e
RoccoRiccardi <Rocco.Riccardi.42@gmail.com>
2015-12-08 15:00:53 -0500

    used dotdotdot plugin to truncate postcontent fields on hom index view. woo.

---
+++ DapperDog/DapperDog/Scripts/app/myDotDotDot.js
@@ -0,0 +1,26 @@
+﻿function dotdotdotCallback(isTruncated, originalContent) {
+    if (!isTruncated) {
+        $("a", this).remove();
+    }
+}
+
+$(document).ready(function () {
+    $(".ellipsis").dotdotdot({
+        after: "a.more",
+        watch: "window",
+        height: 100,
+        callback: dotdotdotCallback
+    });
+    $("div.ellipsis").on('click', 'a', function () {
+        if ($(this).text() === "More") {
+            var div = $(this).closest('div.ellipsis');
+            div.trigger('destroy').find('a.more').hide();
+            div.css('max-height', '');
+            $("a.less", div).show();
+        }
+        else {
+            $(this).hide();
+            $(this).closest('div.ellipsis').css("max-height", "100px").dotdotdot({ after: "a.more", callback: dotdotdotCallback });
+        }
+    });
+});
