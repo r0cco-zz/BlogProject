1deaa773b35a1dd6950e0551217497e9dc038253
Chary Gurney <chary.gurney@gmail.com>
2015-12-08 12:15:07 -0500

    added form validation to edit single static page and add static pages

---
+++ DapperDog/DapperDog/Scripts/app/staticPageValidation.js
@@ -0,0 +1,36 @@
+﻿$.validator.setDefaults({
+    ignore: '',
+    highlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+$(document).ready(function () {
+    $('#addStaticPageForm').validate({
+        rules: {
+            StaticPageTitle: {
+                required: true,
+                maxlength: 100
+            },
+            StaticPageContent: {
+                required: true
+            }
+        }
+    });
+});
+
+$('#submitbutton').click(function () {
+    tinyMCE.triggerSave();
+});
